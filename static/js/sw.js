9ed2490520d8fc2272ad019f792ed50658fa3236
crazyman <rjs9622@outlook.com>
2023-08-09 23:52:31 +0800

    init

---
+++ static/js/sw.js
@@ -0,0 +1,43 @@
+const staticDevCoffee = "dev-coffee-site-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/coffee1.jpg",
+  "/images/coffee2.jpg",
+  "/images/coffee3.jpg",
+  "/images/coffee4.jpg",
+  "/images/coffee5.jpg",
+  "/images/coffee6.jpg",
+  "/images/coffee7.jpg",
+  "/images/coffee8.jpg",
+  "/images/coffee9.jpg",
+];
+
+self.addEventListener("install", (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
+
+// self.addEventListener("activate", async () => {
+//   // This will be called only once when the service worker is activated.
+//   try {
+//     const options = {userVisibleOnly:true};
+//     const subscription = await self.registration.pushManager.subscribe(options);
+//     console.log(JSON.stringify(subscription));
+//   } catch (err) {
+//     console.log("Error", err);
+//   }
+// });
